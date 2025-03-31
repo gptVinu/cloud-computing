19a2713750f63aa252e10250bfa185241a833e5b
Gupta Vinayak Makandar <148940938+gptVinu@users.noreply.github.com>
2025-03-31 11:39:05 +0530

    Create Calculator.java

---
+++ rmi/Calculator.java
@@ -0,0 +1,12 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+/**
+ *
+ * @author vinyak
+ */
+public interface Calculator extends Remote {
+    public int add(int a, int b) throws RemoteException; 
+    public int sub(int a, int b) throws RemoteException;
+    public int mul(int a, int b) throws RemoteException; 
+    public int div(int a, int b) throws RemoteException;
+}
